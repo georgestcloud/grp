ba0ec696115840f276bd35213250cb0e2cf91d99
George Chinaka <132566100+georgestcloud@users.noreply.github.com>
2024-05-28 11:57:51 +0100

    index.js

--- index.js
+++ index.js
@@ -79,3 +79,32 @@ document.querySelector('.image-container').addEventListener('mouseleave', functi
       closeModal(event.target.id);
     }
   }
+
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+  function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+  }
+
+  function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    const modal = document.getElementById('modal2');
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
